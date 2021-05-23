b6b05aefac059c375a8c1e29dced7cb0501e2ee3
satish <sboddukuri@gmail.com>
2021-05-23 05:44:32 -0700

    Initial commit

---
+++ src/pages/homepage/homepage-component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import Directoty from '../../components/directory/directory.component';
+
+const HomePage = () =>
+(
+<div className='homepage'>
+   <Directoty/>
+</div>
+
+)
+
+export default HomePage;
